59af693f1817778257ea50abef7f5e55aeae7462
kolah <kolabalaji2000@gmail.com>
2023-03-01 23:43:12 +0530

    test

--- app/src/main/java/com/example/fme/mainactivity.java
+++ app/src/main/java/com/example/fme/mainactivity.java
@@ -4,8 +4,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
+
 
 public class mainactivity extends Activity {
     Button b1,b2,b3,b4,b5,b6,b7;
@@ -14,76 +15,54 @@ public class mainactivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.depatment);
-        b1 = (Button) findViewById(R.id.b1);
-        b2 = (Button) findViewById(R.id.b2);
-        b3 = (Button) findViewById(R.id.b3);
-        b4 = (Button) findViewById(R.id.b4);
-        b5 = (Button) findViewById(R.id.b5);
-        b6 = (Button) findViewById(R.id.b6);
-        b7 = (Button) findViewById(R.id.b7);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String department=b1.getText().toString();
-                Intent i=new Intent(mainactivity.this,semester.class);
-                i.putExtra("department",department);
-                startActivity(i);
-            }
+        b1 =  findViewById(R.id.b1);
+        b2 =  findViewById(R.id.b2);
+        b3 =  findViewById(R.id.b3);
+        b4 =  findViewById(R.id.b4);
+        b5 =  findViewById(R.id.b5);
+        b6 =  findViewById(R.id.b6);
+        b7 =  findViewById(R.id.b7);
+        b1.setOnClickListener(view -> {
+            String department=b1.getText().toString();
+            Intent i=new Intent(mainactivity.this,semester.class);
+            i.putExtra("department",department);
+            startActivity(i);
         });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String department=b2.getText().toString();
-                Intent i=new Intent(mainactivity.this,semester.class);
-                i.putExtra("department",department);
-                startActivity(i);
-            }
+        b2.setOnClickListener(view -> {
+            String department=b2.getText().toString();
+            Intent i=new Intent(mainactivity.this,semester.class);
+            i.putExtra("department",department);
+            startActivity(i);
         });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String department=b3.getText().toString();
-                Intent i=new Intent(mainactivity.this,semester.class);
-                i.putExtra("department",department);
-                startActivity(i);
-            }
+        b3.setOnClickListener(view -> {
+            String department=b3.getText().toString();
+            Intent i=new Intent(mainactivity.this,semester.class);
+            i.putExtra("department",department);
+            startActivity(i);
         });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String department=b4.getText().toString();
-                Intent i=new Intent(mainactivity.this,semester.class);
-                i.putExtra("department",department);
-                startActivity(i);
-            }
+        b4.setOnClickListener(view -> {
+            String department=b4.getText().toString();
+            Intent i=new Intent(mainactivity.this,semester.class);
+            i.putExtra("department",department);
+            startActivity(i);
         });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String department=b5.getText().toString();
-                Intent i=new Intent(mainactivity.this,semester.class);
-                i.putExtra("department",department);
-                startActivity(i);
-            }
+        b5.setOnClickListener(view -> {
+            String department=b5.getText().toString();
+            Intent i=new Intent(mainactivity.this,semester.class);
+            i.putExtra("department",department);
+            startActivity(i);
         });
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String department=b6.getText().toString();
-                Intent i=new Intent(mainactivity.this,semester.class);
-                i.putExtra("department",department);
-                startActivity(i);
-            }
+        b6.setOnClickListener(view -> {
+            String department=b6.getText().toString();
+            Intent i=new Intent(mainactivity.this,semester.class);
+            i.putExtra("department",department);
+            startActivity(i);
         });
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String department=b7.getText().toString();
-                Intent i=new Intent(mainactivity.this,semester.class);
-                i.putExtra("department",department);
-                startActivity(i);
-            }
+        b7.setOnClickListener(view -> {
+            String department=b7.getText().toString();
+            Intent i=new Intent(mainactivity.this,semester.class);
+            i.putExtra("department",department);
+            startActivity(i);
         });
     }
 
--- app/src/main/java/com/example/fme/semester.java
+++ app/src/main/java/com/example/fme/semester.java
@@ -1,23 +1,86 @@
 package com.example.fme;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class semester extends Activity {
-    TextView t1;
+    public Button b1, b2;
+     public TextView t1;
+     String department;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.semester);
-        t1=(TextView)findViewById(R.id.t1);
-        Bundle i=getIntent().getExtras();
-        String department=i.getString("department");
-        Toast.makeText(this, department, Toast.LENGTH_SHORT).show();
+        Bundle k=getIntent().getExtras();
+        department=k.getString("department");
+        //Toast.makeText(this, department, Toast.LENGTH_SHORT).show();
+        t1=findViewById(R.id.t1);
         t1.setText(department);
+        b1 =  findViewById(R.id.b1);
+        b2 =  findViewById(R.id.b2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            Intent i;
+            String semester;
+            @Override
+            public void onClick(View view) {
+                switch(k.getString("department")){
+                    case "computer science and engineering":
+                        semester=b1.getText().toString();
+                        i = new Intent(semester.this, cse_sem1.class);
+                        i.putExtra("semester",semester);
+                        i.putExtra("department",department);
+                        startActivity(i);
+                        break;
+                    case "cse(ai)":
+                        semester=b1.getText().toString();
+                        i = new Intent(semester.this, cseai_sem1.class);
+                        i.putExtra("semester",semester);
+                        i.putExtra("department",department);
+                        startActivity(i);
+                        break;
+                    case "cse(ds)":
+                        semester=b1.getText().toString();
+                         i = new Intent(semester.this, cseds_sem1.class);
+                        i.putExtra("semester",semester);
+                        i.putExtra("department",department);
+                        startActivity(i);
+                        break;
+                    case "eletronics and communication engineering":
+                        semester=b1.getText().toString();
+                         i = new Intent(semester.this, ece_sem1.class);
+                        i.putExtra("semester",semester);
+                        i.putExtra("department",department);
+                        startActivity(i);
+                        break;
+                    case "electrical and eletronic engineering":
+                        semester=b1.getText().toString();
+                       i = new Intent(semester.this, eee_sem1.class);
+                        i.putExtra("semester",semester);
+                        i.putExtra("department",department);
+                        startActivity(i);
+                        break;
+                    case "mechanical engineering":
+                        semester=b1.getText().toString();
+                        i = new Intent(semester.this, mech_sem1.class);
+                        i.putExtra("semester",semester);
+                        i.putExtra("department",department);
+                        startActivity(i);
+                        break;
+                    case "civil engineering":
+                        semester=b1.getText().toString();
+                         i = new Intent(semester.this, civil_sem1.class);
+                        i.putExtra("semester",semester);
+                        i.putExtra("department",department);
+                        startActivity(i);
+                        break;
+                }
+            }
+        });
+    }
 }
-}
+
